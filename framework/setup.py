809441cbbd62b16abb813acd0675f9ae964d2594
THANHDNV <vanthanh971125@gmail.com>
2018-10-15 23:48:23 +0700

    add some changes

---
+++ framework/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ossec-api",
+    version="1.0.0",
+    description="Control OSSEC with OSSEC API",
+    package=find_packages(),
+    install_requires=[],
+)
